1c95edce8abf4eec6173d7280f60e4661760de8e
efmanu <manuedavakandam@gmail.com>
2021-01-08 00:04:26 +1100

    comments

--- src/MHSampler.jl
+++ src/MHSampler.jl
@@ -32,20 +32,27 @@ states = mh(model, priorPDF, likelihood_dist, data)
 function mh(model, priorPDF, likelihood_dist, data; proposalPDF=priorPDF, itr = 1000)
 	states = Array{Float64}(undef,0)
 	burn_in = Int(itr*0.2)
+	#initial value
 	prev_params = 1.0
-	for i=1:itr
+	for i=2:itr
 		append!(states,prev_params)
 		params = rand(proposalPDF)
 		lg_curr = log_joint(model,priorPDF, params, likelihood_dist, data)	 
 		lg_prev = log_joint(model,priorPDF, prev_params, likelihood_dist, data)	 
 
 		logα = lg_curr - lg_prev
+
+		#acceptance criteria
 		if(-Random.randexp() < logα)
 			prev_params = params
 		end
 	end
 	return states[burn_in:itr-1]
 end
+"""
+	log_joint(model,priorPDF, params, likelihood_dist, data)	
+Function identify log of joint distribution
+"""
 function log_joint(model,priorPDF, params, likelihood_dist, data)	 
 	logpdf_prior = pdf(priorPDF, params)
 	pred = model(params)
